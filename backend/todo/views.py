b1bf61a253bcb3ee8126a8560115799f31a94e84
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-04 20:27:56 +0300

    Mise en place de views et urls pour le views et post

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status, permissions, response, views
+
+from .models import Todo
+from .serializers import TodoSerializer
+
 
 # Create your views here.
+class TodoListApiView(views.APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        todos = Todo.objects.filter(user=request.user.id)
+        serializer = TodoSerializer(todos, many=True)
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+        data = {
+            'task': request.data.get('task'),
+            'completed': request.data.get('completed'),
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
