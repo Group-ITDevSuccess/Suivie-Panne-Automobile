63f959f28dbe73f244642f1adb0df5dc4100f9cb
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-05 13:07:42 +0300

    Rectification de lien de redirction

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
-import {Toaster} from  "react-hot-toast"
+import {Toaster} from "react-hot-toast"
 import router from "./router.jsx";
 import './style.css'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Toaster />
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Toaster/>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <RouterProvider router={router}/>
+        </DevSupport>
+    </React.StrictMode>,
 )
--- frontend/src/pages/accounts/components/GestLayout.jsx
+++ frontend/src/pages/accounts/components/GestLayout.jsx
@@ -1,10 +1,11 @@
 import {useStateContext} from "../../../context/ContextProvider.jsx";
 import { Link, Navigate, Outlet } from 'react-router-dom'
+import {DASHBOARD_URL_NAVIGATE} from "../../../../config.js";
 export const GestLayout = () => {
     const {token} = useStateContext();
     console.log("Use Token", token)
     if (token){
-        return <Navigate to="/" />
+        return <Navigate to={DASHBOARD_URL_NAVIGATE} />
     }
     return (
         <>
