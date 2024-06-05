e83fa4ed6dde7483c73755c0e23fcea895065465
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-05 07:57:34 +0300

    Ajout de style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import './style.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Page Home</h1>
+    },
+    {
+        path: '/accounts',
+        children: [
+            {
+                path: '/accounts/login',
+                element: <h1>Page Login</h1>
+            },
+            {
+                path: '/accounts/register',
+                element: <h1>Page Register</h1>
+            }
+        ]
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
